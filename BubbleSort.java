f734b45ae91f38125a583bc035d95728eb949af0
Water <qq1754479362@163.com>
2022-11-22 14:13:01 +0800

    text commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -7,24 +7,6 @@ public class BubbleSort {
         array[index2] = temp;
     }
 
-    public static long sort(int[] array) {
-        long cont = 0;
-        for (int i = array.length - 1; i >= 0; i--) {
-            boolean haveChange = false;
-            for (int j = array.length - 1; j >= array.length - i; j--) {
-                if (array[j] < array[j - 1]) {
-                    change(j, j - 1, array);
-                    haveChange = true;
-                    cont++;
-                }
-
-            }
-            if (!haveChange) {
-                break;
-            }
-        }
-        return cont;
-    }
 
     public static void main(String[] args) {
         int[] a = {4, 1, 2, 3};
